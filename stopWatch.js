aabad93e8a712b9f6a9a7eee5061e2d88b5dc539
Carlo <carlo.little@googlemail.com>
2019-12-13 23:57:54 +0100

    Add stopWatch class

--- stopWatch.js
+++ stopWatch.js
@@ -1,35 +1,45 @@
-var timeElapsed;
-var displayedTime;
-var laps;
-var status;
+class stopWatch {
+    function constructor() {
+        var laps;
+        var timeElapsed;
+        var displayedTime;
+        var status;
+    }
 
-function startTimer() {
-    laps = [0];
-	time = 0;
-	status = "running";
+    function startTimer() {
+        timeElapsed = 0;
+        laps = [timeElapsed];
+        setInterval(() => {
+            timeElapsed += 1
+        })
+        displayedTime = elapstedTime;
+        status = "running";
+    }
+    
+    function stopTimer() {
+        // TODO: pause timer
+        status = "paused";
+        displayedTime = elapstedTime;
+        displayedTime = timeElapsed;
+    }
+    
+    function pauseTimer(status) {
+        // TODO: pause timer
+        if (status == "paused") {}
+    
+    }
+    
+    function lapTimer() {}
+    
+    function resetLap() {}
+    
+    function calculateTotalTime() {}
+    
+    function calculateAverageTime() {}
+    
+    function calculateFastestLap() {}
+    
+    function displayTime() {}
+    
+    function getFormattedTime() {}
 }
-
-function stopTimer() {
-    status = "paused";
-    displayedTime = timeElapsed;
-}
-
-function pauseTimer(status) {
-    // TODO: pause timer
-	if (status == "paused") {}
-
-}
-
-function lapTimer() {}
-
-function resetLap() {}
-
-function calculateTotalTime() {}
-
-function calculateAverageTime() {}
-
-function calculateFastestLap() {}
-
-function displayTime() {}
-
-function getFormattedTime() {}
