ba3669396578fd1938f6634fcaf2a3b504cdbd6d
Carlo <carlo.little@googlemail.com>
2020-01-21 21:33:28 +0100

    add stopwatch methods

--- stopWatch.js
+++ stopWatch.js
@@ -1,53 +1,87 @@
 class stopWatch {
-    function constructor() {
+    constructor() {
         var laps;
-        var timeElapsed;
+        var timeElapsed = 0;
         var displayedTime;
         var status;
     }
 
-    function startTimer() {
-        timeElapsed = 0;
+    startTimer(status, laps, timeElapsed, displayedTime) {
+        status = "running";
         laps = [timeElapsed];
         var timeId = setInterval(() => {
             timeElapsed += 1
         })
-        displayedTime = elapstedTime;
-        status = "running";
-    }
-    
-    function stopTimer() {
-        // TODO: pause timer
-        status = "paused";
-        displayedTime = elapstedTime;
         displayedTime = timeElapsed;
     }
     
-    function pauseTimer(status) {
+    stopTimer(status, displayedTime, timeElapsed) {
+        status = "stopped";
+        clearInterval(timeId);
+        displayedTime = timeElapsed;
+    }
+    
+    pauseTimer(status, timeElapsed) {
         if (status == "running") {
             clearInterval(timeId);
+            status = "paused";
         } else if (status == "paused") {
             var timeId = setInterval(() => {
                 timeElapsed += 1
-            }
+            })
         }
     }
     
-    function lapTimer() {
+    lapTimer(status, timeElapsed) {
         if (status != "stopped") {
-            laps.push(0);
+            laps.push(timeElapsed);
         }
+        timeElapsed = 0;
     }
     
-    function resetLap() {}
+    resetLap(laps) {
+        laps.splice(-1, 1, 0)
+    }
     
-    function calculateTotalTime() {}
+    calculateTotalTime(laps) {
+        displayedTime = timeElapsed
+        for (lap in laps) {
+            displayedTime += lap
+        }
+        return displayedTime
+    }
     
-    function calculateAverageTime() {}
+    calculateAverageTime(laps) {
+        // ?? Are parameters necessary or are all variables accessible whithin one class?
+        return this.calculateTotalTime(laps) / laps.length
+    }
     
-    function calculateFastestLap() {}
+    calculateFastestLap(laps) {
+        return Math.min(laps)
+    }
     
-    function displayTime() {}
+    displayTime(time) {
+        return this.getFormattedTime(time)
+    }
     
-    function getFormattedTime() {}
+    getFormattedTime(millisec) {
+
+        var seconds = (millisec / 1000).toFixed(1);
+
+        var minutes = (millisec / (1000 * 60)).toFixed(1);
+
+        var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+
+        var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+
+        if (seconds < 60) {
+            return seconds + " Sec";
+        } else if (minutes < 60) {
+            return minutes + " Min";
+        } else if (hours < 24) {
+            return hours + " Hrs";
+        } else {
+            return days + " Days"
+        }
+    }
 }
