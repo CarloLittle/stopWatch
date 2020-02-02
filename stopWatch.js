6ef77a673ed22d0cfb38a7a965826dd17ba10c2e
Nicholas Simons <nicholas.s@webinterpret.com>
2020-02-02 21:18:56 +0100

    add questions

--- stopWatch.js
+++ stopWatch.js
@@ -1,79 +1,74 @@
 class stopWatch {
     constructor() {
-        var laps;
-        var timeElapsed = 0;
-        var displayedTime;
-        var status;
+        var this.laps;              // Are all this. variables accessible within the class (like class-globals?)?
+        var this.timeElapsed = 0;
+        var this.displayedTime;
+        var this.status;
+        var this.timeId;
     }
 
     startTimer(status, laps, timeElapsed, displayedTime) {
-        status = "running";
-        laps = [timeElapsed];
-        var timeId = setInterval(() => {
-            timeElapsed += 1
+        this.status = "running";
+        this.laps = [this.timeElapsed];
+        this.timeId = setInterval(() => {
+            this.timeElapsed += 1
         })
-        displayedTime = timeElapsed;
+        this.displayedTime = this.timeElapsed;
     }
-    
+
     stopTimer(status, displayedTime, timeElapsed) {
-        status = "stopped";
-        clearInterval(timeId);
-        displayedTime = timeElapsed;
+        this.status = "stopped";
+        clearInterval(this.timeId);
+        this.displayedTime = this.timeElapsed;
     }
-    
+
     pauseTimer(status, timeElapsed) {
-        if (status == "running") {
-            clearInterval(timeId);
-            status = "paused";
-        } else if (status == "paused") {
-            var timeId = setInterval(() => {
-                timeElapsed += 1
+        if (this.status == "running") {
+            clearInterval(this.timeId);
+            this.status = "paused";
+        } else if (this.status == "paused") {
+            this.timeId = setInterval(() => { // What if you don't know when a variable is first going to be called? Hoisting?
+                this.timeElapsed += 1
             })
         }
     }
-    
-    lapTimer(status, timeElapsed) {
-        if (status != "stopped") {
-            laps.push(timeElapsed);
+
+    lapTimer(status, timeElapsed) {     // Do I even need to pass in anything as a parameter if I can modify this. ?
+        if (status != "stopped") { // Should I be manipulating status or this.status ?
+            this.laps.push(timeElapsed);
         }
-        timeElapsed = 0;
+        this.timeElapsed = 0; // Do I need to pass in all variables as parameters?
     }
-    
+
     resetLap(laps) {
-        laps.splice(-1, 1, 0)
+        this.laps.splice(-1, 1, 0)
     }
-    
+
     calculateTotalTime(laps) {
-        displayedTime = timeElapsed
-        for (lap in laps) {
-            displayedTime += lap
-        }
+        this.displayedTime = this.timeElapsed
+        laps.forEach(function(lap) {
+            displayedTime = displayedTime + lap
+        })
         return displayedTime
     }
-    
+
     calculateAverageTime(laps) {
         // ?? Are parameters necessary or are all variables accessible whithin one class?
         return this.calculateTotalTime(laps) / laps.length
     }
-    
+
     calculateFastestLap(laps) {
         return Math.min(laps)
     }
-    
+
     displayTime(time) {
-        return this.getFormattedTime(time)
+        return this.getFormattedTime(time) // Are class methods also available under this. ?
     }
-    
     getFormattedTime(millisec) {
-
         var seconds = (millisec / 1000).toFixed(1);
-
         var minutes = (millisec / (1000 * 60)).toFixed(1);
-
         var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-
         var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-
         if (seconds < 60) {
             return seconds + " Sec";
         } else if (minutes < 60) {
@@ -85,3 +80,5 @@ class stopWatch {
         }
     }
 }
+
+module.exports = stopWatch;
