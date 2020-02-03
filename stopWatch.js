be28a4f2797a61cd4bb2cd009756291304cb5e54
CarloLittle <nicholas.s@webinterpret.com>
2020-02-03 23:05:42 +0100

    Refactor stopWatch module and add basic test

--- stopWatch.js
+++ stopWatch.js
@@ -1,13 +1,13 @@
 class stopWatch {
     constructor() {
-        var this.laps;              // Are all this. variables accessible within the class (like class-globals?)?
-        var this.timeElapsed = 0;
-        var this.displayedTime;
-        var this.status;
-        var this.timeId;
+        this.laps = [];
+        this.timeElapsed = 0;
+        this.displayedTime = undefined;
+        this.status = "stopped";
+        this.timeId = undefined;
     }
 
-    startTimer(status, laps, timeElapsed, displayedTime) {
+    startTimer() {
         this.status = "running";
         this.laps = [this.timeElapsed];
         this.timeId = setInterval(() => {
@@ -16,68 +16,59 @@ class stopWatch {
         this.displayedTime = this.timeElapsed;
     }
 
-    stopTimer(status, displayedTime, timeElapsed) {
+    stopTimer() {
         this.status = "stopped";
         clearInterval(this.timeId);
         this.displayedTime = this.timeElapsed;
     }
 
-    pauseTimer(status, timeElapsed) {
+    pauseTimer() {
         if (this.status == "running") {
             clearInterval(this.timeId);
             this.status = "paused";
         } else if (this.status == "paused") {
-            this.timeId = setInterval(() => { // What if you don't know when a variable is first going to be called? Hoisting?
+            this.timeId = setInterval(() => {
                 this.timeElapsed += 1
             })
         }
     }
 
-    lapTimer(status, timeElapsed) {     // Do I even need to pass in anything as a parameter if I can modify this. ?
-        if (status != "stopped") { // Should I be manipulating status or this.status ?
+    lapTimer() {
+        if (status != "stopped") {
             this.laps.push(timeElapsed);
         }
-        this.timeElapsed = 0; // Do I need to pass in all variables as parameters?
+        this.timeElapsed = 0;
     }
 
-    resetLap(laps) {
+    resetLap() {
         this.laps.splice(-1, 1, 0)
     }
 
-    calculateTotalTime(laps) {
+    calculateTotalTime() {
         this.displayedTime = this.timeElapsed
         laps.forEach(function(lap) {
-            displayedTime = displayedTime + lap
+            this.displayedTime = this.displayedTime + lap
         })
-        return displayedTime
+        return this.formatTime(this.displayedTime)
     }
 
-    calculateAverageTime(laps) {
-        // ?? Are parameters necessary or are all variables accessible whithin one class?
-        return this.calculateTotalTime(laps) / laps.length
+    calculateAverageTime() {
+        return this.formatTime(
+            this.calculateTotalTime(laps) / this.laps.length
+        );
     }
 
-    calculateFastestLap(laps) {
-        return Math.min(laps)
+    calculateFastestLap() {
+        return this.formatTime(Math.min(this.laps));
     }
 
-    displayTime(time) {
-        return this.getFormattedTime(time) // Are class methods also available under this. ?
-    }
-    getFormattedTime(millisec) {
-        var seconds = (millisec / 1000).toFixed(1);
-        var minutes = (millisec / (1000 * 60)).toFixed(1);
-        var hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-        var days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-        if (seconds < 60) {
-            return seconds + " Sec";
-        } else if (minutes < 60) {
-            return minutes + " Min";
-        } else if (hours < 24) {
-            return hours + " Hrs";
-        } else {
-            return days + " Days"
-        }
+    formatTime(time) {
+        // Returns time formatted as 01:00:00.001
+        milliseconds = time.splice(-1, 3);
+        seconds = time.splice(-1, 2);
+        minutes = time.splice(-1, 2);
+        hours = time;
+        return `{$hours}:{$minutes}:{$seconds}.{$milliseconds}`
     }
 }
 
---
+++ stopWatch.test.js
@@ -0,0 +1,5 @@
+const stopWatch = require('./stopWatch.js');
+
+test('test code compiles', () => {
+  let casio = new stopWatch();
+});
